c3528e0b352a4af1b88ea3b4504ef55d7f5f3039
Jshot117 <jonnjstart@gmail.com>
2025-02-08 17:40:09 -0500

    Added component and changed home page

--- app/home/page.js
+++ app/home/page.js
@@ -7,14 +7,18 @@ import Image from "next/image"
 import { useState } from "react"
 import { useUser, RedirectToSignIn } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
+import { useEffect } from "react"
+import GoogleSearch from "@/components/ui/GoogleSearch.jsx"
 
 export default function Page() {
-    const { isSignedIn } = useUser(); 
-    const router = useRouter(); 
+    const { isSignedIn } = useUser();
+    const router = useRouter();
 
     if (!isSignedIn) {
         return <RedirectToSignIn />;
     }
+
+
     const [currentMode, setMode] = useState("Search Mode")
     const [userQuery, setQuery] = useState("")
 
@@ -31,35 +35,35 @@ export default function Page() {
     }
 
     const querySubmit = async (e) => {
-        if(e.key === "Enter"){
-            try{
+        if (e.key === "Enter") {
+            try {
                 const response = await fetch("/get_embeddings", {
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify({query: userQuery}),
+                    body: JSON.stringify({ query: userQuery }),
                     method: "POST"
                 })
 
-                if(response.ok){
-                    try{
+                if (response.ok) {
+                    try {
                         const embeddingVal = await response.value
                         const checkCache = await fetch("check_cache", {
                             headers: {
                                 headers: {
                                     "Content-Type": "application/json",
                                 },
-                                body: JSON.stringify({embedding: embeddingVal}),
+                                body: JSON.stringify({ embedding: embeddingVal }),
                                 method: "POST"
                             }
                         })
 
 
-                    }catch(error){
+                    } catch (error) {
 
                     }
                 }
-            }catch(error){
+            } catch (error) {
 
             }
         }
@@ -75,12 +79,18 @@ export default function Page() {
                     alt="coral"
                 />
                 <div className="flex items-center gap-10">
-                    <Input 
-                        onKeyDown={querySubmit} 
-                        onChange = {(e) => changeQuery(e.target.value)}
-                        type="input" 
-                        placeholder="Enter your query"
-                        className="w-[50vw]"/>
+                    {currentMode === "AI Mode" &&
+                        <Input
+                            onKeyDown={querySubmit}
+                            onChange={(e) => changeQuery(e.target.value)}
+                            type="input"
+                            placeholder="Enter your query"
+                            className="w-[50vw]" />
+                    }
+                    {currentMode === "Search Mode" &&
+                            <GoogleSearch/>
+                    }
+
                     <div className="flex gap-2 items-center">
                         <Switch
                             onCheckedChange={changeMode}
