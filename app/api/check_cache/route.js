9891de42ab45bebda7a8e5c07dcd2b4c43e49c41
justinespinal <espinaljustin03@gmail.com>
2025-02-08 15:42:24 -0500

    changes

--- app/api/check_cache/route.js
+++ app/api/check_cache/route.js
@@ -1,3 +1,19 @@
-export async function GET() {
-  return new Response(JSON.stringify({ message: "Hello, World!" }), {});
+import { Index } from "@upstash/vector";
+
+
+
+export async function POST(req) {
+  const index = new Index({
+    url: "",
+    token: ""
+  })
+
+  try{
+    const results = await index.query({
+      data: req.body.query,
+      topK: 1
+    })
+  }catch(e){
+    console.error(e)
+  }
 }
