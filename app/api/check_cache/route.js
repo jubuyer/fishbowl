f3ac68562e3f80d4c01f077cbf38357d2d17e541
rakib-shahid <rakibshahid1298@gmail.com>
2025-02-08 20:34:29 -0500

    fixed cache checks

--- app/api/check_cache/route.js
+++ app/api/check_cache/route.js
@@ -1,32 +1,35 @@
 import { Index } from "@upstash/vector";
 
 export async function POST(request) {
-  const {query} = await request.json()
+  const { query } = await request.json();
 
   const index = new Index({
     url: process.env.UPSTASH_VECTOR_REST_URL,
-    token: process.env.UPSTASH_VECTOR_REST_TOKEN
-  })
+    token: process.env.UPSTASH_VECTOR_REST_TOKEN,
+  });
 
-  try{
+  try {
     const results = await index.query({
       data: query,
-      topK: 1
-    })
+      topK: 1,
+      includeMetadata: true,
+    });
+    const filteredResults = results.filter((item) => item.score > 0.93);
     return new Response(
-      JSON.stringify({result: results.length != 0}), 
+      JSON.stringify({
+        result: filteredResults.length !== 0,
+        response: filteredResults,
+      }),
       {
         status: 200,
-        headers: {"Content-Type": "application/json"}
-      })
-  }catch(e){
-    console.error(e)
-    return new Response(
-      JSON.stringify({message: "did not work!"}), 
-      {
-        status: 500,
-        headers: {"Content-Type": "application/json"}
-      })
-
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    return new Response(JSON.stringify({ message: "did not work!" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
