cea74cba829ef3a4a32fff1660d33cec6a5141db
justinespinal <espinaljustin03@gmail.com>
2025-02-09 01:19:05 -0500

    minecraft

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-blue-500 to-indigo-900 `}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <header className="flex justify-end items-center p-4 gap-4 h-16 minecraft">
             <SignedOut>
               <SignInButton />
               <SignUpButton />
